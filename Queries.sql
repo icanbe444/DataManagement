34c399ffca331f68a12003de39ac86485e93704d
Akanbi Abdullahi <icanbe444@gmail.com>
2023-11-14 01:27:00 +0000

    Create query

---
+++ Queries.sql
@@ -0,0 +1,25 @@
+/*
+Query A: For each kind of pizza, display the ID and name of the pizza, 
+and full details of the chef who cooks the pizza. 
+Order the output by the name of the pizza.
+*/
+
+
+USE Pizza_2213884;
+GO
+
+SELECT P.pizza_id, P.name AS 'Name of Pizza', CONCAT(C.title ,' ' , C.name) AS "Chef's FullName" 
+FROM Pizzas P, Chef C ORDER BY P.name;
+
+/*Query B: For each pizza cooked by chef Cesare, display the ID and name of the pizza 
+and the total cost of all its toppings.
+*/
+
+SELECT P.pizza_id, P.name AS Pizza_Name,C.name, SUM(T.cost) AS 'Total Cost'
+FROM Pizzas P, Chef C,Topping T 
+WHERE P.chef_id = C.chef_id AND  C.name LIKE 'Cesare' GROUP BY P.pizza_id, P.name, C.name; 
+
+
+
+
+
